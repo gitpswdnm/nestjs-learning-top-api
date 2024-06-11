b98a6de8cc66b0b727b8d2576d6501bdcc37cefb
gitpswdnm <kir9877@gmail.com>
2024-06-12 04:38:15 +0500

    fix

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { ProductController } from './product.controller';
-import { ProductService } from './product.service';
 import { ProductModel } from './product.model';
+import { ProductService } from './product.service';
 
 @Module({
 	controllers: [ProductController],
